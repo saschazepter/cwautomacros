960b4d96dc2a06c36067de9d66c7755fcfd99842
Carlo Wood <carlo@alinoe.com>
2006-02-28 20:48:00 +0000

    * doc/index.php: Update documentation.
    git-svn-id: svn://localhost/CW-AutoMacros.svn,repos,svnroot,svn2.berlios.de/trunk@12 6a1ecc81-c4c8-904c-b5a8-411a816c4db3

--- doc/index.php
+++ doc/index.php
@@ -8,16 +8,16 @@
 
 <h2>CWautomacros: a collection of autoconf m4 macros by Carlo Wood</h2>
 
-<p>This project is intended for <em>developers</em> (people who use CVS to download a project) of
-projects that use <i>cwautomacros</i>.</p>
+<p>This project is intended for <em>developers</em> who downloaded a project with CVS/SVN
+that uses <i>cwautomacros</i>.</p>
 
-<p>If you have downloaded a project that sent you here, then
-get the sources with <a href="http://developer.berlios.de/cvs/?group_id=6077">Anonymous CVS Access</a>,
+<p>If you have been sent here after running `autogen.sh', then
+get the cwautomacros sources with <a href="http://developer.berlios.de/cvs/?group_id=6077">Anonymous CVS Access</a>,
 or download the latest <a href="http://developer.berlios.de/project/showfiles.php?group_id=6077">tar ball</a>.</p>
 
-<p>It's rather plug-and-play: Everything is installed in <code>/usr/share/cwautomacros</code>
+<p>It's all plug-and-play: the files and scripts are installed under <code>/usr/share/cwautomacros</code>
 (or in <code>$CWAUTOMACROSPREFIX/share/cwautomacros</code> if you set that environment variable).
-Then simply go back to the project that sent you hear and run it's 'autogen.sh'.</p>
+After installation, simply go back to the project that sent you here and (re-)run it's `autogen.sh' script.</p>
 
 </body>
 </html>
